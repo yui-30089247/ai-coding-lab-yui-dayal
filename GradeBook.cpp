962950d81520446901787998334f84f01f062212
yui-30089247 <30089247@cc.peralta.edu>
2026-05-04 19:13:52 +0000

    Create stubs

---
+++ GradeBook.cpp
@@ -0,0 +1,22 @@
+#include "GradeBook.h"
+
+GradeBook::GradeBook() {
+
+};
+
+void GradeBook::addGrade(const std::string& assignment, double score) {
+    return;
+};
+
+double GradeBook::getAverage() const {
+    return 50.0;
+};
+
+double GradeBook::getHighest() const {
+    return 70.0;
+};
+
+
+int GradeBook::numGrades() const {
+    return 5;
+};
